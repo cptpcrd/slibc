b7bc9761aa2f34637da7f57ed290ce11407f86f6
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-02-05 16:25:19 -0500

    test: improve utsname test and move out of tests/
    It's a simple test that didn't really belong in tests/.

--- src/utsname.rs
+++ src/utsname.rs
@@ -30,3 +30,31 @@ pub fn uname() -> Result<Utsname> {
     Error::unpack_nz(unsafe { libc::uname(buf.as_mut_ptr()) })?;
     Ok(Utsname(unsafe { buf.assume_init() }))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_uname() {
+        let utsname = uname().unwrap();
+
+        #[cfg(target_os = "linux")]
+        assert_eq!(utsname.sysname(), OsStr::new("Linux"));
+        #[cfg(target_os = "freebsd")]
+        assert_eq!(utsname.sysname(), OsStr::new("FreeBSD"));
+
+        let mut buf = [0; 4096];
+        let hostname = crate::gethostname(&mut buf).unwrap();
+        assert_eq!(utsname.nodename(), OsStr::from_bytes(hostname.to_bytes()));
+
+        #[cfg(target_os = "linux")]
+        {
+            let domainname = crate::getdomainname(&mut buf).unwrap();
+            assert_eq!(
+                utsname.domainname(),
+                OsStr::from_bytes(domainname.to_bytes())
+            );
+        }
+    }
+}
