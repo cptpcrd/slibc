04c45d6cba6dcfc51c06d48b49a0eaee4a596a88
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-02-05 11:28:49 -0500

    style: fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -87,9 +87,9 @@ mod internal_prelude {
 
     #[cfg(all(feature = "alloc", not(feature = "std")))]
     pub use alloc::{
-        format,
         borrow::{Cow, ToOwned},
         boxed::Box,
+        format,
         string::{String, ToString},
         vec::Vec,
     };
