ff5bf9180047d6898362c9c6dc745a6ed3c0b135
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-02-14 17:35:07 -0500

    meta: cfg(linux) -> cfg(linux_like)
    Since clippy complains about cfg(linux).

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     match std::env::var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
-        "linux" => println!("cargo:rustc-cfg=linux"),
+        "linux" | "android" => println!("cargo:rustc-cfg=linux_like"),
         "freebsd" | "dragonfly" => println!("cargo:rustc-cfg=bsd\ncargo:rustc-cfg=freebsdlike"),
         "netbsd" | "openbsd" => println!("cargo:rustc-cfg=bsd\ncargo:rustc-cfg=netbsdlike"),
         "macos" | "ios" => println!("cargo:rustc-cfg=bsd\ncargo:rustc-cfg=apple"),
