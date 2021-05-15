5c160fb574f5b90a0e051337bb9d491720ac3937
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-05-15 11:45:11 -0400

    feat(string): add memcmp() wrappers

--- src/string.rs
+++ src/string.rs
@@ -1,3 +1,5 @@
+use core::cmp::Ordering;
+
 /// Search a given byte slice for a given byte.
 ///
 /// This is a simple wrapper around the system's `memchr()` function. For more advanced uses,
@@ -48,6 +50,39 @@ pub fn memrchr(s: &[u8], c: u8) -> Option<usize> {
     }
 }
 
+/// Compare two memory areas.
+///
+/// See `memcmp(2)`.
+///
+/// # Safety
+///
+/// Both `s1` and `s2` must be valid for reads of size `n` bytes.
+#[inline]
+pub unsafe fn memcmp_raw(s1: *const u8, s2: *const u8, n: usize) -> Ordering {
+    libc::memcmp(s1 as *const _, s2 as *const _, n).cmp(&0)
+}
+
+/// Compare two byte slices.
+///
+/// This is a safe version of [`memcmp_raw()`]. If `s1` and `s2` are different lengths, it returns
+/// an `Ordering` comparing their lengths.
+///
+/// # Example
+///
+/// ```
+/// # use core::cmp::Ordering;
+/// # use slibc::memcmp;
+/// assert_eq!(memcmp(b"", b""), Ordering::Equal);
+/// assert_eq!(memcmp(b"a", b"b"), Ordering::Less);
+/// assert_eq!(memcmp(b"ab", b"b"), Ordering::Greater);
+/// ```
+#[inline]
+pub fn memcmp(s1: &[u8], s2: &[u8]) -> Ordering {
+    s1.len()
+        .cmp(&s2.len())
+        .then_with(|| unsafe { memcmp_raw(s1.as_ptr(), s2.as_ptr(), s1.len()) })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -89,5 +124,37 @@ mod tests {
 
         assert_eq!(memrchr(b"", b'a'), None);
         assert_eq!(memrchr(b"", b'\0'), None);
+    }
+
+    #[test]
+    fn test_memcmp_raw() {
+        unsafe {
+            assert_eq!(memcmp_raw(b"".as_ptr(), b"".as_ptr(), 0), Ordering::Equal);
+            assert_eq!(
+                memcmp_raw(b"abc".as_ptr(), b"abc".as_ptr(), 3),
+                Ordering::Equal
+            );
+
+            assert_eq!(
+                memcmp_raw(b"abc".as_ptr(), b"abd".as_ptr(), 3),
+                Ordering::Less
+            );
+            assert_eq!(
+                memcmp_raw(b"zbc".as_ptr(), b"abc".as_ptr(), 3),
+                Ordering::Greater
+            );
+        }
+    }
+
+    #[test]
+    fn test_memcmp() {
+        assert_eq!(memcmp(b"", b""), Ordering::Equal);
+        assert_eq!(memcmp(b"abc", b"abc"), Ordering::Equal);
+
+        assert_eq!(memcmp(b"a", b"b"), Ordering::Less);
+        assert_eq!(memcmp(b"abcd", b"abcc"), Ordering::Greater);
+
+        assert_eq!(memcmp(b"ab", b"b"), Ordering::Greater);
+        assert_eq!(memcmp(b"ab", b"abc"), Ordering::Less);
     }
 }
