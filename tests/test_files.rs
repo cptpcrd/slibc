e71a505dc1350d10967736fffb4be774bebb8a41
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-06-05 10:23:15 -0400

    test: combine 'cwd' test into 'files' test

--- tests/test_files.rs
+++ tests/test_files.rs
@@ -54,3 +54,68 @@ fn test_open_openat() {
 
     assert_eq!(file.read_exact(&mut [0; 1]).unwrap_err(), Errno::EINVAL);
 }
+
+#[cfg(feature = "alloc")]
+#[test]
+fn test_chdir_getcwd() {
+    use slibc::{chdir, fchdir, getcwd, getcwd_alloc, PATH_MAX};
+
+    let mut buf = [0; PATH_MAX];
+
+    // chdir()
+
+    chdir("/").unwrap();
+    assert_eq!(getcwd(&mut buf).unwrap().to_bytes(), b"/");
+    assert_eq!(getcwd_alloc().unwrap().to_bytes(), b"/");
+
+    chdir("/bin").unwrap();
+    assert_eq!(
+        getcwd(&mut buf).unwrap().to_bytes(),
+        std::fs::canonicalize("/bin")
+            .unwrap()
+            .as_os_str()
+            .as_bytes()
+    );
+    assert_eq!(
+        getcwd_alloc().unwrap().to_bytes(),
+        std::fs::canonicalize("/bin")
+            .unwrap()
+            .as_os_str()
+            .as_bytes()
+    );
+
+    // fchdir()
+
+    let f = open(
+        "/",
+        OFlag::O_RDONLY | OFlag::O_DIRECTORY | OFlag::O_CLOEXEC,
+        0,
+    )
+    .unwrap();
+    fchdir(f.fd()).unwrap();
+    assert_eq!(getcwd(&mut buf).unwrap().to_bytes(), b"/");
+    assert_eq!(getcwd_alloc().unwrap().to_bytes(), b"/");
+
+    let f = open(
+        "/bin",
+        OFlag::O_RDONLY | OFlag::O_DIRECTORY | OFlag::O_CLOEXEC,
+        0,
+    )
+    .unwrap();
+    fchdir(f.fd()).unwrap();
+    assert_eq!(
+        getcwd(&mut buf).unwrap().to_bytes(),
+        std::fs::canonicalize("/bin")
+            .unwrap()
+            .as_os_str()
+            .as_bytes()
+    );
+    assert_eq!(
+        getcwd_alloc().unwrap().to_bytes(),
+        std::fs::canonicalize("/bin")
+            .unwrap()
+            .as_os_str()
+            .as_bytes()
+    );
+}
+
