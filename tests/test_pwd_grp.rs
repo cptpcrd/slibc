ea239654169baadb2102dc221ba2051a4c6dc4be
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-06-05 10:17:19 -0400

    test: combine pwd and grp tests
    For speed; each integration test file adds more time to running 'cargo
    test' or 'cargo tarpaulin'. Because of the ways they conflict, some of
    them can't be combined, but these two can.

--- tests/test_pwd_grp.rs
+++ tests/test_pwd_grp.rs
@@ -2,14 +2,23 @@
 // macOS behaves strangely in situations like this
 #![cfg(not(target_os = "macos"))]
 
-use slibc::{Passwd, PasswdIter};
+use slibc::{Group, GroupIter, Passwd, PasswdIter};
 
 #[cfg(feature = "std")]
-fn hash(pwd: &Passwd) -> u64 {
+fn hash_pwd(pwd: &Passwd) -> u64 {
     use core::hash::{Hash, Hasher};
 
     let mut hasher = std::collections::hash_map::DefaultHasher::new();
     pwd.hash(&mut hasher);
+    hasher.finish()
+}
+
+#[cfg(feature = "std")]
+fn hash_grp(grp: &Group) -> u64 {
+    use core::hash::{Hash, Hasher};
+
+    let mut hasher = std::collections::hash_map::DefaultHasher::new();
+    grp.hash(&mut hasher);
     hasher.finish()
 }
 
@@ -41,7 +50,7 @@ fn test_passwd_iter() {
         assert_eq!(pwd, pwd.clone());
 
         #[cfg(feature = "std")]
-        assert_eq!(hash(&pwd), hash(&pwd.clone()));
+        assert_eq!(hash_pwd(&pwd), hash_pwd(&pwd.clone()));
 
         if duplicate_names.iter().find(|&p| p == pwd.name()).is_none() {
             // Look up by name and make sure we get the same result
@@ -50,7 +59,7 @@ fn test_passwd_iter() {
             assert_eq!(format!("{:?}", pwd), format!("{:?}", pwd2));
 
             #[cfg(feature = "std")]
-            assert_eq!(hash(&pwd), hash(&pwd2));
+            assert_eq!(hash_pwd(&pwd), hash_pwd(&pwd2));
         }
 
         if !duplicate_uids.contains(&pwd.uid()) {
@@ -60,7 +69,50 @@ fn test_passwd_iter() {
             assert_eq!(format!("{:?}", pwd), format!("{:?}", pwd3));
 
             #[cfg(feature = "std")]
-            assert_eq!(hash(&pwd), hash(&pwd3));
+            assert_eq!(hash_pwd(&pwd), hash_pwd(&pwd3));
+        }
+    }
+}
+
+#[test]
+fn test_group_iter() {
+    let mut groups: Vec<Group> = Vec::new();
+    let mut duplicate_gids = Vec::new();
+
+    for group in unsafe { GroupIter::new() } {
+        let group = group.unwrap();
+        if groups.iter().any(|g| g.gid() == group.gid()) {
+            duplicate_gids.push(group.gid());
+        }
+        groups.push(group);
+    }
+
+    duplicate_gids.sort_unstable();
+    duplicate_gids.dedup();
+
+    for grp in groups {
+        assert_eq!(format!("{:?}", grp), format!("{:?}", grp.clone()));
+        assert_eq!(grp, grp.clone());
+
+        #[cfg(feature = "std")]
+        assert_eq!(hash_grp(&grp), hash_grp(&grp.clone()));
+
+        // Look up by name and make sure we get the same result
+        let grp2 = Group::lookup_name(grp.name()).unwrap().unwrap();
+        assert_eq!(grp, grp2);
+        assert_eq!(format!("{:?}", grp), format!("{:?}", grp2));
+
+        #[cfg(feature = "std")]
+        assert_eq!(hash_grp(&grp), hash_grp(&grp2));
+
+        if !duplicate_gids.contains(&grp.gid()) {
+            // Look up by GID and make sure we get the same result
+            let grp3 = Group::lookup_gid(grp.gid()).unwrap().unwrap();
+            assert_eq!(grp, grp3);
+            assert_eq!(format!("{:?}", grp), format!("{:?}", grp3));
+
+            #[cfg(feature = "std")]
+            assert_eq!(hash_grp(&grp), hash_grp(&grp3));
         }
     }
 }
